08be0a351d4ad8cb14d1e226ecf2edb663f95f9a
どきみき <lmznd6yiew@gmail.com>
2023-04-07 00:42:55 +0900

    APIサーバーのテストを作成

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":3939",
+	}
+
+	http.HandleFunc("/", Hello)
+	server.ListenAndServe()
+}
