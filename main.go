8321e5efddf4b86cfcede430f056f894e8a241ac
kikki28 <kikki.28.n@gmail.com>
2023-04-30 12:40:34 +0900

    HandleFuncの検証

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Kou kou!"))
 }
-
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/end", Hello) // /が来たときに func Hello を実行する
 
 	err := http.ListenAndServeTLS(":3939", "ncth-app.jp.pem", "ncth-app.jp.key", nil)
+	//func ListenAndServeTLS(addr, certFile, keyFile string, handler Handler)
+
 	if err != nil {
 		fmt.Printf("ERROR : %s", err)
 	}
