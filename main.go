a55a40ab3f60dbb24a0e03c03d7dfba47a03e24a
kikki28 <kikki.28.n@gmail.com>
2023-05-30 06:54:31 +0900

    クエリパラメータの取得

--- main.go
+++ main.go
@@ -13,18 +13,38 @@ func hello(c echo.Context) error {
 
 func main() {
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/", hello)
 	e.GET("/user/:id", GetId)
+
+	e.GET("/usio", GetQueryParam)
+	println("うしおこうは おしまい!!%d", 1)
 	e.Logger.Fatal(e.Start(":3939"))
+
 }
 
 // パスパラメータを受け取って返すらしい
 func GetId(c echo.Context) error {
 	// ここでパスパラメータを受け取ってる
-	id := c.Param("id")
+	id := "うしおこう" //c.Param("id")
 	return c.String(http.StatusOK, id)
+}
+
+func GetQueryParam(c echo.Context) error {
+
+	//クエリパラメータの取得
+	sort := c.QueryParam("sort")
+	limit := c.QueryParam("limit")
+
+	//map型 map[キーの型名]値の型名{キー:値, キー:値, ...}
+	//interface{}型はint, float, string などさまざまな型の値を格納できるが、演算はできない
+	res := map[string]interface{}{
+		"sort":  sort,
+		"limit": limit,
+	}
+
+	//JSONで返す
+	return c.JSON(http.StatusOK, res)
 }
